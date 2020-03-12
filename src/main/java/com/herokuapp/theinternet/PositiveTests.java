414431cbf7b0e78580fb038dc2d52796fa42c5ec
Alex <akuzin20@gmail.com>
2020-03-12 11:18:49 +0300

    commit v3 tests for webelements added

--- src/main/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/main/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,13 +1,15 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class PositiveTests {
 
     @Test
-    public void loginTest(){
+    public void loginTest() {
         System.out.println("Starting loginTest");
 
         //create driver
@@ -23,13 +25,21 @@ public class PositiveTests {
         System.out.println("Page is opened");
 
 //        enter username
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
 //        enter password
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword!");
 //        click login button
+        WebElement logInButton = driver.findElement(By.tagName("button"));
+        logInButton.click();
 //
 //        verifications:
 //        new url
 //        logout button is visible
+        WebElement logOutButton = driver.findElement(By.xpath("//a[@class='button secondary radius']"));
         //success login  message
+        WebElement successMessage = driver.findElement(By.cssSelector("#flash"));
 
         //Close browser
         driver.quit();
